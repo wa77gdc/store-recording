a99e06fe5a881172ad30f29795ff4bc6e4f8ade8
wa77gdc <wa77gdc@gmail.com>
2019-11-04 14:40:57 +0700

    first commit

---
+++ src/components/Rut.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Rut extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Rut</h3>
+            </div>
+        )
+    }
+}
